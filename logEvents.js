fdb79b0d1aa96d30dcc5e7ab12204437572187fd
ViniTamanhao <105015723+ViniTamanhao@users.noreply.github.com>
2022-10-07 16:03:54 -0300

    commiting main changes

--- logEvents.js
+++ logEvents.js
@@ -1,21 +1,20 @@
 const { format } = require("date-fns");
 const { v4: uuid } = require("uuid");
+
 const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-const logEvents = async (message) => {
-  var dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
-  var logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-  console.log(logItem);
+const logEvents = async (message, logName) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
   try {
     if (!fs.existsSync(path.join(__dirname, "logs"))) {
       await fsPromises.mkdir(path.join(__dirname, "logs"));
     }
-    await fsPromises.appendFile(
-      path.join(__dirname, "logs", "eventLog.txt"),
-      logItem
-    );
+
+    await fsPromises.appendFile(path.join(__dirname, "logs", logName), logItem);
   } catch (err) {
     console.log(err);
   }
