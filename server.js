5fe98d5eb78a9af208cabf06ef3fedfdf0cd3d74
ViniTamanhao <105015723+ViniTamanhao@users.noreply.github.com>
2022-10-17 15:18:12 -0300

    cleaning up and adding controllers

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
 const PORT = process.env.PORT || 3500;
@@ -9,56 +10,30 @@ const PORT = process.env.PORT || 3500;
 // custom middleware logger
 app.use(logger);
 
-// cors stands for "Cross Origin Resource Sharing"
-const whitelist = [
-  "https://www.yoursite.com",
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) != -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("not allowed by CORS"));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
+// Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
-// built-in middleware to handle urlenconded data
+// built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
-// serve static files
-app.use(express.static(path.join(__dirname, "/public")));
-app.use("/subdir", express.static(path.join(__dirname, "/public")));
+// built-in middleware for json
+app.use(express.json());
 
-// using routes
+//serve static files
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+// routes
 app.use("/", require("./routes/root"));
-app.use("/subdir", require("./routes/subdir"));
-
-app.get("^/$|/index(.html)?", (req, res) => {
-  //res.sendFile('./views/index.html', { root: __dirname });
-  res.sendFile(path.join(__dirname, "views", "index.html"));
-});
-
-app.get("/new-page(.html)?", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "new-page.html"));
-});
-
-app.get("/old-page(.html)?", (req, res) => {
-  res.redirect(301, "/new-page.html"); //302 by default
-});
+app.use("/employees", require("./routes/api/employees"));
 
 app.all("*", (req, res) => {
   res.status(404);
   if (req.accepts("html")) {
     res.sendFile(path.join(__dirname, "views", "404.html"));
   } else if (req.accepts("json")) {
-    res.json({ error: "404 page not found" });
+    res.json({ error: "404 Not Found" });
   } else {
-    res.type("txt").send("404 not found");
+    res.type("txt").send("404 Not Found");
   }
 });
 
