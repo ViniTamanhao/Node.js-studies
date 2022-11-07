c3ba610c99ac0bf7ec15256d35f0c45d1b07a79d
ViniTamanhao <105015723+ViniTamanhao@users.noreply.github.com>
2022-11-07 16:23:56 -0300

    adding cookies and token authentication

---
+++ middleware/credentials.js
@@ -0,0 +1,12 @@
+const { es } = require("date-fns/locale");
+const allowedOrigins = require("../config/allowedOrigins");
+
+const credentials = (req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Credentials", true);
+  }
+  next();
+};
+
+module.exports = credentials;
